6409035b4140c06894f6d2e08b90d362e52bd444
ddcdc03 <xiqiang.ch@gmail.com>
2024-11-07 16:39:53 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,10 @@ export let PORT = 370; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXYS: Proxy[] = [];
+export let PROXYS: Proxy[] = [
+      {
+        domain: "easy-reverse-proxy-pied.vercel.app",
+        url: "https://www.mysms.one/api/",
+        enable: true
+    }
+];

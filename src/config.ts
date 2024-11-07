96df5b29cfbfefd7cd5989bb5ca36c079204e7d7
ddcdc03 <xiqiang.ch@gmail.com>
2024-11-07 16:55:20 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXYS: Proxy[] = [
       {
-        domain: "ussms-git-patch-1-ddcdc03s-projects.vercel.app",
+        domain: "ussms.vercel.app",
         url: "http://sms222.us",
         enable: true
     }
